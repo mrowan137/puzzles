e64ac26981389841e8925e2a9f228f1caed29513
mrowan137 <mrowan137@gmail.com>
2023-01-24 00:14:22 -0800

    7

--- project_euler/7.py
+++ project_euler/7.py
@@ -3,7 +3,7 @@ Q: By listing the first six prime numbers:
 
   2, 3, 5, 7, 11, and 13,
 
-we can see that the 6th prime is 13. What is the 10 001st prime number?
+we can see that the 6th prime is 13. What is the 10001st prime number?
 
 A: 104743
 """
