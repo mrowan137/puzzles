e2403e4a08399b13c59273305dc42cbb1c45b0e5
Michael Rowan <mrowan137@gmail.com>
2023-06-02 17:17:27 -0700

    66

--- project_euler/66.py
+++ project_euler/66.py
@@ -17,7 +17,7 @@ obtained when D = 5.
 Find the value of D in minimal solutions of x for which the largest value of x
 is obtained.
 
-A: 
+A: 661
 """
 
 # Given:
@@ -27,57 +27,105 @@ A:
 # D is a given positive integer;
 # Find minimal solution in x.
 #
-# Quadratic formula:
-#   x = sqrt( 1 + D*y^2 )
-#
-# Let's test it out:
-#   D = 2:
-#   x = sqrt( 1 + 2*y^2 )
-#
-# Count upward from y = 1 shows x = sqrt(9) = 3 is the minimal solution.
-# So we can count from y = 1 until get to x is an integer.
-# It will be the minimal since sqrt(n) is monotonically increasing.
+# Brute force takes too long.
+# Special equation, 'Pell Equation', which can be solved using continued
+# fraction for sqrt(D).
+# - https://mathworld.wolfram.com/DiophantineEquation2ndPowers.html
+# - https://math.uchicago.edu/~may/VIGRE/VIGRE2008/REUPapers/Yang.pdf
 
-import decimal
-from math import sqrt
+from math import sqrt, isqrt
 
 
-decimal.getcontext().prec = 20
+def continued_fraction_representation_sqrt(D):
+    """
+    return continued fraction representation of sqrt(D)
+    [a0, <a1, a2, ..., am>]
+    """
+    rep = []
+    # starting point:
+    #   b = a0, p = 1, sD := sqrt(D)
+    #   p/( sD - b )
+    #
+    # complete the square:
+    #   p*(sD + b)/ (sD**2 - b**2)
+    #
+    # simplify:
+    #   (sD + b) / ((sD**2 - b**2)/p)
+    #
+    # extract next terms and iterate:
+    #   p' = (sD**2 - b**2)/p
+    #   b' = int(p/(sD - b))*p' - b
+    sD = sqrt(D)
+    a0 = int(sD)
+    a = b = a0
+    p = 1
+
+    # done if 0 fractional part (i.e. D is a perfect square),
+    # or if we reached the last term of the periodic part, 2*a0
+    while a not in (sD, 2 * a0):
+        rep.append(a)
+        a = int(p / (sD - b))
+        p = (D - b**2) / p
+        b = a * p - b
+
+    # the last term
+    rep += [a0] if a == sD else [2 * a0]
+
+    return rep
 
 
-def diophantine(D):
-    largest_x = -float("inf")
-    res = -1
-    for d in range(2, D + 1):
-        # no solutions in positive integers when d is square
-        if sqrt(d) == int(sqrt(d)):
+def pell_equation_solution(D):
+    """
+    return solution to Pell equation of form x^2 - D*y^2 = 1
+    """
+
+    # by assumption, no solution for D perfect square
+    if isqrt(D) ** 2 == D:
+        return None
+
+    cf = continued_fraction_representation_sqrt(D)
+
+    # initialize
+    p0, p1 = 1, cf[0]
+    q0, q1 = 0, 1
+
+    # for period length l, minimal solution is given by
+    #           { p_l-1, q_l-1,   l even
+    # (x, y) =  |
+    #           { p_2l-1, q_2l-1, l odd
+    for a in cf[1:] + cf[1:]:
+        p1, p0 = a * p1 + p0, p1
+        q1, q0 = a * q1 + q0, q1
+
+        if p0**2 - D * q0**2 == 1:
+            return p0, q0
+
+    return None
+
+
+def answer(upper):
+    res, largest_x = -1, -float("inf")
+
+    for d in range(upper):
+        soln = pell_equation_solution(d)
+
+        if not soln:
             continue
 
-        y = decimal.Decimal(1)
-        while True:
-            y += decimal.Decimal(1)
-            x_squared = (
-                decimal.Decimal(1)
-                + decimal.Decimal(d) * decimal.Decimal(y) ** 2
-            )
-
-            if int(sqrt(x_squared)) ** 2 == x_squared:
-                break
+        x, y = soln
 
         old = largest_x
-        largest_x = max(sqrt(x_squared), largest_x)
+        largest_x = max(x, largest_x)
         res = d if old != largest_x else res
 
-        print(
-            f"d = {d}, x = {sqrt(x_squared)}, y = {y}, x^2 - D*y^2 = {x_squared - d*y**2}"
-        )
+        print(f"d = {d}, x = {x}, y = {y}, x^2 - D*y^2 = {x**2 - d*y**2}")
 
     return res
 
 
 if __name__ == "__main__":
-    upper = 1000
+    UPPER = 1000
     print(
-        f"For D <= {upper}, D = {diophantine(upper)}"
+        f"For D <= {UPPER}, D = {answer(UPPER)}"
         + " gives largest x of minimal solutions in x, for x^2 - D*y^2 = 1."
     )
