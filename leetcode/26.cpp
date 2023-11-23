dc35587932a2c4eb7d3b6a1d894747061c048a69
Michael Rowan <mrowan137@gmail.com>
2023-11-23 09:05:50 -0800

    26.cpp

--- leetcode/26.cpp
+++ leetcode/26.cpp
@@ -1,15 +1,27 @@
-// Runtime: 11 ms, faster than 89.61% of C++ online submissions for Remove Duplicates from Sorted Array.
-// Memory Usage: 18.4 MB, less than 36.66% of C++ online submissions for Remove Duplicates from Sorted Array.
+// Runtime: 7 ms
+// Memory Usage: 18.8 MB
 class Solution {
  public:
-  int removeDuplicates(vector<int> &nums) {
-    int i = 0;
-    for (int j = 0; j < nums.size(); ++j) {
-      if (i == 0 || nums[i - 1] != nums[j]) {
-        nums[i] = nums[j];
-        i += 1;
-      }
+    int removeDuplicates(vector<int>& nums) {
+        // two pointer approach
+        int i = 0, j = 0;
+
+        // i writes the non-duplicate value
+        // j reads non-duplicate value
+        while (j < nums.size()) {
+            // j points to non-duplicate value
+            nums[i] = nums[j];
+
+            // point i to the next write location
+            i += 1;
+
+            // increment j to the next non-duplicate
+            j += 1;
+            while (j < nums.size() && nums[j] == nums[j-1]) {
+                j += 1;
+            }
+        }
+
+        return i;
     }
-    return i;
-  }
 };
