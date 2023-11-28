b4d6914ff64c05401fc4b2e66278a185678820f9
Michael Rowan <mrowan137@gmail.com>
2023-11-27 18:13:45 -0800

    136.cpp

--- leetcode/136.cpp
+++ leetcode/136.cpp
@@ -1,17 +1,19 @@
-// Runtime: 31 ms, faster than 53.23% of C++ online submissions for Single Number.
-// Memory Usage: 17 MB, less than 51.79% of C++ online submissions for Single Number.
+// Runtime: 16 ms
+// Memory Usage: 17.2 MB
+
+/*
+  O(n) iterate through array.
+*/
 class Solution {
  public:
-  int singleNumber(vector<int> &nums) {
-    // use a smart propert of XOR: x^x = 0
-    // => only the singleton remain if we xor everything
-    // note also 0 ^ T = T, 0 ^ F = F => 0 ^ x = x
-    // so 0 can be the initial
-    int res = 0;
-    for (vector<int>::const_iterator it = nums.begin(); it != nums.end();
-         ++it) {
-      res ^= *it;
+  int singleNumber(vector<int>& nums) {
+    // use a sneaky property of XOR:
+    // a XOR a XOR b = b
+    int res = nums[0];
+    for (size_t i = 1; i < nums.size(); ++i) {
+      res ^= nums[i];
     }
+
     return res;
   }
 };
