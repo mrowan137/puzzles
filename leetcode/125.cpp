8e9bc17f1f749561021dfd8b5a284404770b7d92
Michael Rowan <mrowan137@gmail.com>
2023-12-08 10:28:46 -0800

    125.cpp

--- leetcode/125.cpp
+++ leetcode/125.cpp
@@ -1,3 +1,31 @@
+// Runtime: 9 ms
+// Memory Usage: 7.7 MB
+class Solution {
+ public:
+  bool isPalindrome(string s) {
+    // remove non-alphanumeric characters
+    string::iterator erase_start = std::remove_if(
+      s.begin(),
+      s.end(), 
+      [](const unsigned char& c){ return !std::isalnum(c); }
+    );  // returns iterator to start of 'delete' range 
+    s.erase(erase_start, s.end());  // do actual deletion
+    std::transform(
+      s.begin(), s.end(),  // transform range
+      s.begin(),           // start storing transformed
+      [](const unsigned char& c){ return std::tolower(c); }
+    );
+    // check remaining is palindrome
+    string::const_iterator l = s.begin(), r = s.end() - 1;
+    while (l < r) {
+      if (*l != *r) return false;
+      ++l;
+      --r;
+    }
+    return true;
+  }
+};
+
 // Runtime: 6 ms, faster than 69.22% of C++ online submissions for Valid Palindrome.
 // Memory Usage: 7.4 MB, less than 51.53% of C++ online submissions for Valid Palindrome.
 #include <cctype>
