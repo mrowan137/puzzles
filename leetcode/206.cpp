3c048f747e96931868f469a24a13252a7fb7ad8a
Michael Rowan <mrowan137@gmail.com>
2023-12-11 08:32:59 -0800

    206.cpp

--- leetcode/206.cpp
+++ leetcode/206.cpp
@@ -1,3 +1,35 @@
+// Runtime: 8 ms
+// Memory Usage: 9.1 MB
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  void Insert(ListNode* curr, ListNode* nxt) {
+    ListNode* tmp = curr->next;
+    curr->next = nxt;
+    nxt->next = tmp;
+    return;
+  }
+  ListNode* reverseList(ListNode* head) {
+    ListNode* curr = head;
+    ListNode* rev = new ListNode(-1);
+    while (curr != nullptr) {
+      ListNode* insert = new ListNode(curr->val);
+      Insert(rev, insert);
+      curr = curr->next;
+    }
+    return rev->next;
+  }
+};
+
 // Runtime: 34 ms, faster than 5.22% of C++ online submissions for Reverse Linked List.
 // Memory Usage: 8.5 MB, less than 10.70% of C++ online submissions for Reverse Linked List.
 // iterative, in-place
