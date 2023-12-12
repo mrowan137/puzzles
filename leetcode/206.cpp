324d77e25cad3c2f6161154a53182063a92aac49
Michael Rowan <mrowan137@gmail.com>
2023-12-12 08:36:52 -0800

    206.cpp

--- leetcode/206.cpp
+++ leetcode/206.cpp
@@ -1,5 +1,3 @@
-// Runtime: 8 ms
-// Memory Usage: 9.1 MB
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,6 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// Runtime: 7 ms
+// Memory Usage: 8.8 MB
+// iterative, in-place
+class Solution {
+ public:
+  ListNode* reverseList(ListNode* head) {
+    ListNode* rev = nullptr;
+    while (head != nullptr) {
+      ListNode* nxt = head->next;
+      head->next = rev;
+      rev = head;
+      head = nxt;
+    }
+    return rev;
+  }
+};
+
+// Runtime: 8 ms
+// Memory Usage: 9.1 MB
+// iterative, not in-place
 class Solution {
  public:
   void Insert(ListNode* curr, ListNode* nxt) {
@@ -27,23 +45,6 @@ class Solution {
       curr = curr->next;
     }
     return rev->next;
-  }
-};
-
-// Runtime: 34 ms, faster than 5.22% of C++ online submissions for Reverse Linked List.
-// Memory Usage: 8.5 MB, less than 10.70% of C++ online submissions for Reverse Linked List.
-// iterative, in-place
-class Solution {
- public:
-  ListNode *reverseList(ListNode *head) {
-    ListNode *reversed = nullptr;
-    while (head) {
-      ListNode *next = head->next;
-      head->next = reversed;
-      reversed = head;
-      head = next;
-    }
-    return reversed;
   }
 };
 
@@ -67,13 +68,3 @@ class Solution {
     return reverse;
   }
 };
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
