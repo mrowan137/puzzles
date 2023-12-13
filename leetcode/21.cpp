94fc122bdfad60384c854cbceb9612dcb22293b2
Michael Rowan <mrowan137@gmail.com>
2023-12-13 12:11:38 -0800

    21.cpp

--- leetcode/21.cpp
+++ leetcode/21.cpp
@@ -1,5 +1,5 @@
-// Runtime: 15 ms, faster than 35.22% of C++ online submissions for Merge Two Sorted Lists.
-// Memory Usage: 14.9 MB, less than 42.51% of C++ online submissions for Merge Two Sorted Lists.
+// Runtime: 7 ms
+// Memory Usage: 15.1 MB
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,6 +10,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Solution {
+ public:
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* dummy = new ListNode(-1);
+    ListNode* curr = dummy;
+    while (list1 != nullptr && list2 != nullptr) {
+      if (list1->val < list2->val) {
+        curr->next = list1;
+        list1 = list1->next;
+      } else {
+        curr->next = list2;
+        list2 = list2->next;
+      } 
+      curr = curr->next;
+    }
+    curr->next = list1 != nullptr? list1 : list2;
+    return dummy->next;
+  }
+};
+
+// Runtime: 15 ms, faster than 35.22% of C++ online submissions for Merge Two Sorted Lists.
+// Memory Usage: 14.9 MB, less than 42.51% of C++ online submissions for Merge Two Sorted Lists.
 class Solution {
  public:
   ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
