3cde27c8de0b985b7a811b834ceaba234bb9dc50
Michael Rowan <mrowan137@gmail.com>
2023-12-19 02:19:40 -0800

    189.cpp comments

--- leetcode/189.cpp
+++ leetcode/189.cpp
@@ -3,18 +3,27 @@
 class Solution {
  public:
   void rotate(vector<int>& nums, int k) {
+    // initialize
     int placed = 0;
     int mod_class = 0;
     int idx = 0;
     int write = nums[idx];
     const int n = nums.size();
+
+    // cycle through elements of each mod class
     while (placed < n) {
+      // increment, save, write, and load the next write
       idx += k;
       idx %= n;
       int save = nums[idx];
       nums[idx] = write;
       write = save;
+
+      // value written into correct spot, increment count
       placed += 1;
+
+      // if we loop back to first spot, we shifted all elements
+      // in the current mod cycle and can move to the next one
       if (idx == mod_class) {
         mod_class += 1;
         idx = mod_class;
