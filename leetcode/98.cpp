f3f7e30cb31c4a597ba4b91995e568befde714fb
Michael Rowan <mrowan137@gmail.com>
2023-12-26 16:17:14 -0800

    98.cpp

--- leetcode/98.cpp
+++ leetcode/98.cpp
@@ -6,16 +6,17 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 // Runtime: 12 ms
 // Memory Usage: 22 MB
 class Solution {
  public:
-  bool isValidBST(TreeNode* root, 
-                  long min=static_cast<long>(INT_MIN) - 1,
-                  long max=static_cast<long>(INT_MAX) + 1) {
+  bool isValidBST(TreeNode* root,
+                  int64_t min = static_cast<int64_t>(INT_MIN) - 1,
+                  int64_t max = static_cast<int64_t>(INT_MAX) + 1) {
     // What is a BST? A tree where each node satisfies property:
     // - node value is greater than each value in left subtree;
     // - node value is less than each value in right subtree;
@@ -25,18 +26,16 @@ class Solution {
 
     // 2. general: node is BST if
     //   - left is BST
-    bool left_is_valid_bst = isValidBST(root->left, 
-                                        min, 
-                                        std::min(static_cast<long>(root->val), max));
+    bool left_is_valid_bst = isValidBST(
+        root->left, min, std::min(static_cast<int64_t>(root->val), max));
 
     //   - right is BST
-    bool right_is_valid_bst = isValidBST(root->right,
-                                         std::max(static_cast<long>(root->val), min),
-                                         max);
+    bool right_is_valid_bst = isValidBST(
+        root->right, std::max(static_cast<int64_t>(root->val), min), max);
 
     //   - left_max < node.value < right_min, and
     bool curr_gt_min_lt_max = (min < root->val && root->val < max);
 
     return left_is_valid_bst && right_is_valid_bst && curr_gt_min_lt_max;
-    }
+  }
 };
